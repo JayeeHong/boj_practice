f7654cc13f653dc7adf99fb06d46a293f81832f3
JayeeHong <hjy48486@naver.com>
2023-11-11 10:41:36 +0900

    10807

---
+++ src/step4/Q10807.java
@@ -0,0 +1,25 @@
+package step4;
+
+import java.util.Scanner;
+
+public class Q10807 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int v = sc.nextInt();
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            if (v == arr[i]) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
